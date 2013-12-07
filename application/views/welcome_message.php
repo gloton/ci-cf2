1072fa77b8e381afde0456481a52f6e748d700ae
Gloton <gloton@gmail.com>
2013-12-07 00:43:10 -0500

    Video 4 - Se explica el MVC y como agregar un parametro en el controlador y mostrarlo en la vista

---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso de codigo facilitoh1</title>
+
+	
+</head>
+<body>
+<h1>Hola mundo</h1>
+<h2><?php echo $string; ?></h2>
+
+</body>
+</html>
